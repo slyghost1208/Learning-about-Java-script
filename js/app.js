cef3e9597809448cdab642edc9747a3e8efd67d1
unknown <egbuonutochukwu205@gmail.com>
2024-08-15 15:28:30 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(function(){
+    $('.conmtainer') .hover(fuction(){
+          $(this).css({"borderRadius":"20px","backgroundColor": "green"})
+    }.function(){
+        $(this).css({"borderRadius":"20px","backgroundcolor":"black"})
+    
+        $('button').on('click',function(){
+            $('.container').fadeOut(1000)
+        })
+
+
+        
+        $('btn1').on('click',function(){
+       
+        })
+       
+        $('btn2').on('click',function(){
+           if ( $("div1 > b").text == "Happy"){
+                $("div1 > b"    }
+        })
+        
+       });
+    
+    
+
+ 
